ed634fc07c8f01b607c3915b7e8c11f1a133fe8f
FahimAhmed270 <56820604+FahimAhmed270@users.noreply.github.com>
2023-06-02 20:01:57 +0600

    fetch data

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import './Home.scss'
+import React, { useEffect } from 'react'
+import MovieApi from '../../common/api/MovieApi'
+import { APIKey } from '../../common/api/MovieApiKey'
+import MovieListing from '../MovieListing/MovieListing'
+import {useDispatch} from 'react-redux'
+import { addMovies } from '../../features/movies/movieSlice'
+
 
 const Home = () => {
+  const movieText = 'Harry'
+  const dispatch = useDispatch()
+
+
+  useEffect(()=>{
+    const fetchMovie = async ()=>{
+      const response = await MovieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+      .catch((err)=>{
+        console.log('Error getting movie', err);
+      })
+      dispatch(addMovies(response.data))
+    }
+    fetchMovie()
+  },[])
+
+  
   return (
-    <div>
-      Home
-    </div>
+    <>
+    <div className='banner-img'></div>
+    <MovieListing/> 
+    </>
   )
 }
 
